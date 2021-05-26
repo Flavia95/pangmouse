df8f498cef90e99849d2e1de83358bc0a44e202c
Flavia Villani <flavia.villani14@gmail.com>
2021-05-26 09:36:58 +0200

    add tool for check precision

--- script/statspangvsgeno.sh
+++ script/statspangvsgeno.sh
@@ -16,6 +16,10 @@ vcftools --gzvcf $2 --out pang --hist-indel-len
 #Positive and negative
 gatk CreateSequenceDictionary -R $3 -O UCSC_mm10_chr19_only.dict
 gatk Concordance -R $3  --truth $1 --evaluation $2 --summary summarygatkconcordance.tsv
+#Another tool for check positive and negative
+rtg format -o ref.sdf UCSC_mm10_chr19_only.fa #create a dict of a Reference Genome
+rtg vcfeval -b DBA_2J_consensus_indels+sites_copy_hom_only_chr19.sort.vcf.gz -c chr19_DBA2Jall.pan+ref.norm.sort.onlyDBA2J.filter.withoutN.vcf.gz  -t ref.sdf -o test --sample "DBA2_J,DBA_2J"
+
 
 ####bonus_stats
 vcftools --gzvcf $1 --freq2 --out freq_geno --max-alleles 2
