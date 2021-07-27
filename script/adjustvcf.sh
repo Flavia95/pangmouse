1850dcdfa600cad9fad2d93d4aba0260dd21c85e
Flavia Villani <flavia.villani14@gmail.com>
2021-07-27 15:20:46 +0200

    Create adjustvcf.sh

---
+++ script/adjustvcf.sh
@@ -0,0 +1,19 @@
+#After normalized and genomic VCF 
+#bcftools norm --multiallelics -both -f /home/davida/UCSC_mm10.fa genomic.vcf -o Merged_gvcf_files_autosomes_Feb_2021_recalibrated_all_variants_PASSED.recode.norm.de.vcf Merged_gvcf_files_autosomes_Feb_2021_recalibrated_all_variants_PASSED.recode.vcf.gz
+
+echo "Genomic VCF: $1"
+
+#Reheader Genomic VCF
+bcftools reheader -s listrename.txt  $1 -o Merged_gvcf_files_autosomes_Feb_2021_recalibrated_all_variants_PASSED.reheader.vcf.gz
+
+#Remove spanning deletions
+zgrep -v "*" Merged_gvcf_files_autosomes_Feb_2021_recalibrated_all_variants_PASSED.reheader.vcf.gz | /home/flaviav/tools/bgzip > Merged_gvcf_files_autosomes_Feb_2021_recalibrated_all_variants_PASSED.filter.vcf.gz && /home/flaviav/tools/tabix Merged_gvcf_files_autosomes_Feb_2021_recalibrated_all_variants_PASSED.filter.vcf.gz
+
+#Extract only chr19
+bcftools view Merged_gvcf_files_autosomes_Feb_2021_recalibrated_all_variants_PASSED.filter.vcf.gz --regions chr19 > Merged_gvcf_files_autosomes_Feb_2021_recalibrated_all_variants_PASSED.filter.chr19.vcf
+
+#Remove samples which are not assembled
+vcftools --remove removeind.txt --vcf Merged_gvcf_files_autosomes_Feb_2021_recalibrated_all_variants_PASSED.filter.chr19.vcf --recode --out chr19_removsamples/Merged_gvcf_files_autosomes_Feb_2021_recalibrated_all_variants_PASSED.filter.chr19.removeind.vcf
+
+#Remove sites ref/ref
+bcftools view -c1 Merged_gvcf_files_autosomes_Feb_2021_recalibrated_all_variants_PASSED.filter.chr19.removeind.vcf > chr19_removsamples/Merged_gvcf_files_autosomes_Feb_2021_recalibrated_all_variants_PASSED.filter.chr19.removeind_sites.vcf
